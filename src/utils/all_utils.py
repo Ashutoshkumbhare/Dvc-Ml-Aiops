6682f9b42377da8ea1bc79cfcf1a18d2550b2753
Ashutosh Kumbhare <ashutoshkumbhare07@gmail.com>
2023-05-12 17:05:57 +0530

    stage-1 created

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -1,6 +1,11 @@
 import yaml
 import os
 
+def create_directory(dirs: list):
+    for dir_path in dirs:
+        os.makedirs(dir_path, exist_ok=True)
+        print(f"directory is created at {dir_path}")
+
 def read_yaml(path_to_yaml: str) -> dict:
     with open(path_to_yaml) as yaml_file:
         content = yaml.safe_load(yaml_file)
